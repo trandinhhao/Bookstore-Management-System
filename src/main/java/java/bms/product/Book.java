8dcecb78e6cbc5fc030ed2ab503d38847736e97c
trandinhhao <PC@192.168.0.4>
2024-09-22 23:39:01 +0700

    Test

---
+++ src/main/java/java/bms/product/Book.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java.bms.product;
+
+/**
+ *
+ * @author PC
+ */
+public class Book {
+    public static void main(String[] args) {
+        System.out.println("HaoTest");
+    }
+}
